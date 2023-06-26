3af2320ffd4b54306473d520b2f2d0d5617eff26
DGuskov <denis180391@yandex.ru>
2023-06-26 21:59:47 +0300

    added task08

---
+++ Seminar_1/Task_08/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.Write("Введите натуральное число ");
+int N = int.Parse(Console.ReadLine());
+int index = -N;
+while (index < N)
+{
+    if (index % 2 == 0)
+    {
+        Console.Write($"{index}, ");
+    }
+    index++;
+}
